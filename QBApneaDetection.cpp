6b4ee5ec87b4b4f9c25c1c31af80e3bfe2f8ff03
angieluca <angieluca8@gmail.com>
2025-02-05 12:25:42 -0500

    Fixed counting dif types

--- QBApneaDetection.cpp
+++ QBApneaDetection.cpp
@@ -6,25 +6,20 @@ using namespace std;
 
 int main() 
 { 
-    std::cout<<"Hello World\n"; 
+    /**
+     * 0 = quiet breath; 1 = sigh; 2 = sniff; 3 = apnea; 4 = hypopnea
+     * Should find Type I: 2, Type II: 1, Type III: 2
+     * Type 1 Apnea = sigh 5 or less breath cycles before apnea; Type 2 Apnea = sigh immediately before apnea; Type 3 Apnea: spontaneous and in quiet breathing section
+     * vector<int> breaths: vector representing a segment of diffrent breath types
+     * unordered_map<int, int> apneaMap: stores <apnea index, apnea type>; default apnea type is type 3
+     */
 
     std::vector<int> breaths= {0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3}; 
-
-    //0 = quiet breath; 1 = sigh; 2 = sniff; 3 = apnea; 4 = hypopnea 
-    //Should find Type II: 1, Type I: 2, Type 3: 2 
-
-    int type1 = 0, type2 = 0, type3 = 0;
-    //vector<int> apneas; //store indices of apneas found 
-    unordered_map<int, int> apneaMap; //stores <apnea index, apnea type>; default apnea type is type 3
-
-    std::cout << "The number of Type 1: " << type1 << std::endl; 
-    std::cout << "The number of Type 2: " << type2 << std::endl; 
-    std::cout << "The number of Type 3: " << type3 << std::endl; 
-
+    unordered_map<int, int> apneaMap; 
+    int type1 = 0, type2 = 0, type3 = 0; 
+    
     //Traverse entire list to find apneas O(n)
-    for (const int breath: breaths){ if (breaths[breath] == 3) apneaMap[breath] = 3;} //add apnea index with default type 3
-
-//!! STOPPED HERE BCS NUMBER OF TYPES WASNT CHANGING!!
+    for (int i = 0; i < breaths.size(); i++){ if (breaths[i] == 3) apneaMap[i] = 3;} //add apnea index with default type 3
 
     //Traverse through found apneas to classify them
     for (auto apneaPair : apneaMap)
@@ -52,7 +47,6 @@ int main()
         //If not changed to type 2 nor type 1, then remains as default type 3
         type3 = apneaMap.size() - type1 - type2;
     }
-
   
     std::cout << "The number of Type 1: " << type1 << std::endl; 
     std::cout << "The number of Type 2: " << type2 << std::endl; 
